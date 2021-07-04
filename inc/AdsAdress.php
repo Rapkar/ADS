f82e01fa95f8e21c4a81aea8be770ec1f1f6de6e
hosseinsoltanian <33098879+Rapkar@users.noreply.github.com>
2021-07-04 13:59:47 +0430

    Update AdsAdress.php

--- inc/AdsAdress.php
+++ inc/AdsAdress.php
@@ -1,8 +1,8 @@
 <?php
 //change permalink to meta box link
-add_filter( 'post_type_link', 'my_custom_permalinks', 10, 2 );
-function my_custom_permalinks( $permalink, $post ) {
+add_filter( 'post_type_link', 'utc_ads_custom_permalinks', 10, 2 );
+function utc_ads_custom_permalinks( $permalink, $post ) {
     $value = get_post_meta( $post->ID, '_utechia_Adress_meta_key', true );
-    $value= esc_html($value);
+    $value= esc_url($value);
       return str_replace(  $permalink,$value, $permalink );
 }
