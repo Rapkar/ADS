2094ac8be100830810146c67768cda3856206338
hosseinsoltanian <33098879+Rapkar@users.noreply.github.com>
2021-07-04 13:50:17 +0430

    Update AdsElementor.php

--- inc/elementor/AdsElementor.php
+++ inc/elementor/AdsElementor.php
@@ -1,5 +1,7 @@
 <?php
+
 use Elementor\Widget_Base;
+
 class AdsElementor extends Widget_Base
 {
 
@@ -21,51 +23,56 @@ class AdsElementor extends Widget_Base
         return 'fa fa-gg-circle';
     }
 
-    protected function _register_controls() {
+    protected function _register_controls()
+    {
 
         $this->start_controls_section(
             'content_section',
             [
 
-                'label' => __( 'Content', '$this->domain' ),
+                'label' => __('Content', '$this->domain'),
                 'tab' => \Elementor\Controls_Manager::TAB_CONTENT,
 
             ]
         );
 
-        $args = array('post_type' => 'ads',
-            'post_status' => 'publish');
+        $args = array(
+            'post_type' => 'ads',
+            'post_status' => 'publish'
+        );
 
-        $loops = new WP_Query( $args ); 
-        $options=[];
-        $adverts=$loops->get_posts($args);
-        foreach ($adverts as $key=>$item){
-        $options[$item->ID]=$item->post_title;
-          
+        $loops = new WP_Query($args);
+        $options = [];
+        $adverts = $loops->get_posts($args);
+        foreach ($adverts as $key => $item) {
+            $options[$item->ID] = $item->post_title;
         }
-		$this->add_control(
-			'AdsElimgID',
-			[
-				'label' => __( 'ADS', 'plugin-domain' ),
-				'type' => \Elementor\Controls_Manager::SELECT2,
-				'multiple' => false,
-                'options' =>   $options ,
-			]
-		);
+        $this->add_control(
+            'AdsElimgID',
+            [
+                'label' => __('ADS', 'plugin-domain'),
+                'type' => \Elementor\Controls_Manager::SELECT2,
+                'multiple' => false,
+                'options' =>   $options,
+            ]
+        );
         $this->end_controls_section();
-
     }
 
     protected function render()
     {
         $id = $this->get_settings_for_display('AdsElimgID');
-        $post=get_post($id);
-        $img=get_the_post_thumbnail_url($id);
-        $url = get_post_meta( $post->ID, '_utechia_Adress_meta_key', true );
-        ?>
-        <a class="widget-utechia-ads" href="<?= $url; ?>" post-id="<?= $id ; ?>">
-        <img class="img-fluid" src="<?= $img ?>"></img>
-        </a>
-        <?php
+        $post = get_post($id);
+        $img = get_the_post_thumbnail_url($id);
+        $url = get_post_meta($post->ID, '_utechia_Adress_meta_key', true);
+?>
+        <?php if (has_post_thumbnail($post)) : ?>
+            <a class="widget-utechia-ads" href="<?= $url; ?>" post-id="<?= $id; ?>">
+                <img class="img-fluid" src="<?= $img ?>"></img>
+            </a>
+
+        <?php  endif; ?>
+
+<?php
     }
 }
